361ce57ab9d636e84a956808c6c5c3806e2bbffa
Szomor János <jszomor@gmail.com>
2021-04-16 17:57:18 +0200

    refactor

--- IndustrialUnitDatabase/Model/IndustrialUnitContext.cs
+++ IndustrialUnitDatabase/Model/IndustrialUnitContext.cs
@@ -6,14 +6,14 @@ namespace IndustrialUnitDatabase.Model
   public class IndustrialUnitContext : DbContext
   {
     public IndustrialUnitContext(DbContextOptions<IndustrialUnitContext> options) : base(options) { }
-    public DbSet<Equipment> _equipments { get; set; }
-    public DbSet<Valve> _valves { get; set; }
-    public DbSet<Instrument> _instruments { get; set; }
-    public DbSet<User> _users { get; set; }
-    public DbSet<Order> _order { get; set; }
+    public DbSet<Equipment> Equipments { get; set; }
+    public DbSet<Valve> Valves { get; set; }
+    public DbSet<Instrument> Instruments { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Order { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-      optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IndustrialUnitDb;Integrated Security=True");
+      optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=IndustrialUnitDb;Trusted_Connection=True;MultipleActiveResultSets=true;");
     }
   }
 }
