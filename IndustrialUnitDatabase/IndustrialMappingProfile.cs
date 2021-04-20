396a220cd5775a98d8dc0eba8b9590b0f2336914
Szomor János <jszomor@gmail.com>
2021-04-20 14:01:20 +0200

    IndustrialMappingProfile

---
+++ IndustrialUnitDatabase/IndustrialMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using IndustrialUnitDatabase.Model;
+using IndustrialUnitProvider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndustrialUnitDatabase
+{
+  public class IndustrialMappingProfile: Profile
+  {
+    public IndustrialMappingProfile()
+    {
+      CreateMap<Equipment, EquipmentView>()
+        .ForMember(e => e.Id, ex => ex.MapFrom(e => e.Id))
+        .ReverseMap();
+
+      CreateMap<Valve, ValveView>()
+        .ForMember(e => e.Id, ex => ex.MapFrom(e => e.Id))
+        .ReverseMap();
+
+      CreateMap<Instrument, InstrumentView>()
+        .ForMember(e => e.Id, ex => ex.MapFrom(e => e.Id))
+        .ReverseMap();
+    }
+  }
+}
