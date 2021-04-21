0459ec6a2e01874fb9383b8f7bad757071d7be6b
Szomor János <jszomor@gmail.com>
2021-04-21 09:00:02 +0200

    shit happened

---
+++ IndustrialUnitDatabase/SQLiteDataAccess.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Text;
+
+namespace IndustrialUnitDatabase
+{
+  public class SQLiteDataAccess
+  {
+    public static List<T> LoadPeople<T>()
+    {
+      using(IDbConnection cnn = new SQLiteConnection(""))
+      {
+        cnn.Execute("");
+      }
+
+      return null;
+    }
+  }
+}
