37c647b10e0f707428eb6da2da23d6e15f6352ed
Szomor János <jszomor@gmail.com>
2021-05-14 08:55:53 +0200

    XAML practice

--- IndustrialUnit.WpfUI/ViewModels/ValveViewModel.cs
+++ IndustrialUnit.WpfUI/ViewModels/ValveViewModel.cs
@@ -26,13 +26,13 @@ namespace IndustrialUnit.WpfUI.ViewModels
       }
     }
 
-    public string ItemType { get; set; }
-    public string Operation { get; set; }
-    public decimal Size { get; set; }
-    public string ConnectionType { get; set; }
-    public string Supplier { get; set; }
-    public string Manufacturer { get; set; }
-    public decimal UnitPrice { get; set; }
+    //public string ItemType { get; set; }
+    //public string Operation { get; set; }
+    //public decimal Size { get; set; }
+    //public string ConnectionType { get; set; }
+    //public string Supplier { get; set; }
+    //public string Manufacturer { get; set; }
+    //public decimal UnitPrice { get; set; }
 
     public ICommand LoadValveCommand { get; set; }
 
